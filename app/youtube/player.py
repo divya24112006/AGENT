a02414af83bfb05bc29d8c8b2e336f6409a1f104
divya24112006 <divya24112006@gmail.com>
2026-09-08 12:43:29 +0530

    Update player.py

--- app/youtube/player.py
+++ app/youtube/player.py
@@ -1,1 +1,58 @@
+import re
+import urllib.parse
+import urllib.request
+
+
+def get_vid(query):
+
+  try:
+    encoded + urllib.parse.quote(query)
+
+    url = (
+      "https://ww.youtube.com/results"
+      "?search_query=" + encoded
+    )
+
+    request = urllib.request.Requests(
+       url,
+       headers={
+         "User-agent": "Mozilla/5.0"
+      }
+    )
+
+data = urllib.request.urlopen9
+      request,
+      timeout=5
+).read().decode("utf-8", errors="ignore")
+
+ids = re.findall(
+       r' "videoId":([^"]+)" ',
+       data
+)
+
+return ids[0] id ids else None
+
+except Exception:
+return None
+
+
+def create_youtube_url(command):
+
+  text = command lower() strip()
+
+patterns = [
+  r"play\s+song\s+(.+)",
+  r"play\s+music\s+(.+)",
+  r"play\s+(.+)",
+  r"youtube\s+(.+)"
+]
+
+query = command 
+
+for pattern in patterns:
+
+  match = re.search(
+    pattern,
+    text
+  )
 
